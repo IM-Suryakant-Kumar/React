b0ba5f10093567d555eb85c76e68193256780316
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-03-12 15:39:23 +0530

    L2: Fetching and useEffect

---
+++ Factory.md
@@ -0,0 +1,3 @@
+* You can send data to your children as props as values
+* You can get data from your children to parent using props via sent as function
+* A component only rerenders when it's state will update
--- L3-formsApiReqAndRouting/demmo-app/src/App.js
+++ L3-formsApiReqAndRouting/demmo-app/src/App.js
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import InputBox from "./component/InputBox";
-import OutputBox from "./component/OutputBox";
+import axios from "axios";
+import InputBox from "./Component/todo/InputBox";
+import OutputBox from "./Component/todo/OutputBox";
 
 function App() {
    const [items, setItems] = useState([]);
@@ -21,6 +22,37 @@ function App() {
          <OutputBox list={items}></OutputBox>
       </div>
    );
+   //*      ********************/
+   // let [users, setUsers] = useState([]);
+
+   // useEffect(function () {
+   //    // API request 
+   //    (async () => {
+   //       let res = await axios.get(
+   //          "https://jsonplaceholder.typicode.com/users"
+   //       );
+   //       setUsers(res.data);
+   //    })();
+   // }, []);
+   // console.log("I will execute");
+   // return (
+   //    <div>
+   //       <h1>Fetching Data</h1>
+   //       {/* Conditional rendering */}
+   //       {users.length === 0 ? (
+   //          <h2>...Loading</h2>
+   //       ) : (
+   //          <>
+   //             <h2>Users</h2>
+   //             <ul>
+   //                {users.map((user) => {
+   //                   return <li key={user.id}>{user.name}</li>;
+   //                })}
+   //             </ul>
+   //          </>
+   //       )}
+   //    </div>
+   // );
 }
 
 export default App;
