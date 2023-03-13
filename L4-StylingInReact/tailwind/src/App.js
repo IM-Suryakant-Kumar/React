9e6b1a62aa3f1b6a11f4128145e7201adac2762e
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-03-13 10:46:38 +0530

    L3: Install and use Tailwind

---
+++ L4-StylingInReact/tailwind/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+   return (
+      <div className="App">
+         <h1 className="text-xl 
+         text-blue-400 
+         font-bold
+         ">
+          Hello world!
+          </h1>
+      </div>
+   );
+}
+
+export default App;
