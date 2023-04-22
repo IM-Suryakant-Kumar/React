58ac8e286b513678838d1b4ddafb8a449d219d58
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-04-22 07:54:39 +0530

    Practice: react-router

---
+++ L13-ReactRouterDemo/react-router-demo/src/components/Users.jsx
@@ -0,0 +1,27 @@
+import { Link, Outlet, useSearchParams } from "react-router-dom";
+
+const users = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const showActiveUsers = searchParams.get("filter");
+  return (
+    <div>
+      <h1>user 1</h1>
+      <h1>user 2</h1>
+      <h1>user 3</h1>
+      <Outlet />
+      <div>
+        <button onClick={() => setSearchParams({ filter: "active" })}>
+          Active Users
+        </button>
+        <button onClick={() => setSearchParams({})}>Reset Filter</button>
+      </div>
+      {showActiveUsers ? (
+        <h2>Showing active users</h2>
+      ) : (
+        <h2>Showing all users</h2>
+      )}
+    </div>
+  );
+};
+
+export default users;
