b25a4e213613a947653d6403cb8dbe91fc077fc9
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-04-30 02:33:53 +0530

    Added 05_lession

---
+++ L15-ReduxPractice/05_lession/src/main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+
+import App from "./App.jsx";
+import "./index.css";
+import { fetchUsers } from "./features/users/usersSlice";
+
+store.dispatch(fetchUsers());
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+   <React.StrictMode>
+      <Provider store={store}>
+         <App />
+      </Provider>
+   </React.StrictMode>
+);
